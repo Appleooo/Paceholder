f9d0e302d889c5b92335978923561f4dd5ef202f
Yijing Chen <a1024507613@gmail.com>
2022-03-23 12:31:42 -0500

    implemented facebook login func

--- Paceholder_code/components/navigation/AuthProvider.js
+++ Paceholder_code/components/navigation/AuthProvider.js
@@ -46,31 +46,31 @@ export const AuthProvider = (props) => {
                         console.log(e);
                     }
                 },
-                // facebookLogin: async () => {
-                //     try {
-                //         // Attempt login with permissions
-                //         const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+                facebookLogin: async () => {
+                    try {
+                        // Attempt login with permissions
+                        const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
 
-                //         if (result.isCancelled) {
-                //             throw 'User cancelled the login process';
-                //         }
+                        if (result.isCancelled) {
+                            throw 'User cancelled the login process';
+                        }
 
-                //         // Once signed in, get the users AccesToken
-                //         const data = await AccessToken.getCurrentAccessToken();
+                        // Once signed in, get the users AccesToken
+                        const data = await AccessToken.getCurrentAccessToken();
 
-                //         if (!data) {
-                //             throw 'Something went wrong obtaining access token';
-                //         }
+                        if (!data) {
+                            throw 'Something went wrong obtaining access token';
+                        }
 
-                //         // Create a Firebase credential with the AccessToken
-                //         const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+                        // Create a Firebase credential with the AccessToken
+                        const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
 
-                //         // Sign-in the user with the credential
-                //         return await auth().signInWithCredential(facebookCredential);
-                //     } catch (e) {
-                //         // console.log(e);
-                //     }
-                // }
+                        // Sign-in the user with the credential
+                        return await auth().signInWithCredential(facebookCredential);
+                    } catch (e) {
+                        // console.log(e);
+                    }
+                }
             }}>
             {props.children}
         </AuthContext.Provider>
